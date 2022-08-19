569923c63afc9808b0fc3cf9df68b6f973bc5a8f
Romaric Zounlome <rzounlom@gmail.com>
2022-08-19 00:31:46 -0400

    add toggle functionality to nav items

--- client/src/modules/nav/SideNav.tsx
+++ client/src/modules/nav/SideNav.tsx
@@ -1,37 +1,56 @@
 import { AiFillHeart, AiOutlineShoppingCart } from "react-icons/ai";
 import { ImList2, ImStatsBars } from "react-icons/im";
 import { NavTabs, handleNavTab } from "./navSlice";
+import { useAppDispatch, useAppSelector } from "@app/hooks";
 
 import { FC } from "react";
 import { FaHistory } from "react-icons/fa";
 import styles from "@styles/SideNav.module.scss";
-import { useAppDispatch } from "@app/hooks";
 
 const SideNav: FC = () => {
   const dispatch = useAppDispatch();
+  const currentTab = useAppSelector((state) => state.nav.currentTab);
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
         <AiFillHeart color="#F9A109" />
       </div>
       <div className={styles.navItems}>
-        <ImList2
-          onClick={() => dispatch(handleNavTab(NavTabs.shoppingList))}
-          color="#454545"
-          size={20}
-        />
+        <div
+          className={`${styles.navItem} ${
+            currentTab === NavTabs.shoppingList ? styles.active : ""
+          }`}
+        >
+          <ImList2
+            onClick={() => dispatch(handleNavTab(NavTabs.shoppingList))}
+            color="#454545"
+            size={20}
+          />
+        </div>
 
-        <FaHistory
-          onClick={() => dispatch(handleNavTab(NavTabs.shoppingHistory))}
-          color="#454545"
-          size={20}
-        />
+        <div
+          className={`${styles.navItem} ${
+            currentTab === NavTabs.shoppingHistory ? styles.active : ""
+          }`}
+        >
+          <FaHistory
+            onClick={() => dispatch(handleNavTab(NavTabs.shoppingHistory))}
+            color="#454545"
+            size={20}
+          />
+        </div>
 
-        <ImStatsBars
-          onClick={() => dispatch(handleNavTab(NavTabs.shoppingStats))}
-          color="#454545"
-          size={20}
-        />
+        <div
+          className={`${styles.navItem} ${
+            currentTab === NavTabs.shoppingStats ? styles.active : ""
+          }`}
+        >
+          <ImStatsBars
+            onClick={() => dispatch(handleNavTab(NavTabs.shoppingStats))}
+            color="#454545"
+            size={20}
+          />
+        </div>
       </div>
       <div className={styles.cart}>
         <AiOutlineShoppingCart color="white" size={20} />
