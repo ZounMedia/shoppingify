526a4551dbc9d5d1d24b7b3eca2b218236b1ef74
Romaric Zounlome <rzounlom@gmail.com>
2022-08-18 21:33:16 -0400

    add basUrl and paths to tsconfig

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import type { NextPage } from "next";
 import SideNav from "../common/components/nav/SideNav";
-import styles from "../styles/Home.module.scss";
+import styles from "@styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
