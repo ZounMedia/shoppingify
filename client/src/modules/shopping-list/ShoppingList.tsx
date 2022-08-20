59d35c3730e109f0d63c565d46b1e0068939325b
Romaric Zounlome <rzounlom@gmail.com>
2022-08-20 12:27:18 -0400

    complete shopping list styles

--- client/src/modules/shopping-list/ShoppingList.tsx
+++ client/src/modules/shopping-list/ShoppingList.tsx
@@ -4,13 +4,21 @@ import { shoppingItems } from "./data";
 import styles from "@styles/ShoppingList.module.scss";
 
 const ShoppingList: FC = () => {
+  const categories = ["Fruits and Vegitables", "Meat and Fish", "Beverages"];
+
   return (
     <div className={styles.container}>
-      <CategoryList category="Fruits and Vegitables" items={shoppingItems} />
-      <CategoryList category="Meat and Fish" items={shoppingItems} />
-      <CategoryList category="Beverages" items={shoppingItems} />
+      {categories.map((category: string, idx: number) => (
+        <CategoryList key={idx} category={category} items={shoppingItems} />
+      ))}
     </div>
   );
 };
 
 export default ShoppingList;
+
+{
+  /* <CategoryList category="Fruits and Vegitables" items={shoppingItems} />
+      <CategoryList category="Meat and Fish" items={shoppingItems} />
+      <CategoryList category="Beverages" items={shoppingItems} /> */
+}
