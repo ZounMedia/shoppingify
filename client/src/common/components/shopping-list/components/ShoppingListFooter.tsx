88a7e737cbbaa2dff78ad5309ac64c69ccad4e97
Romaric Zounlome <rzounlom@gmail.com>
2022-08-25 19:50:14 -0400

    add shopping list footer styles

--- client/src/common/components/shopping-list/components/ShoppingListFooter.tsx
+++ client/src/common/components/shopping-list/components/ShoppingListFooter.tsx
@@ -2,7 +2,14 @@ import { FC } from "react";
 import styles from "@styles/ShoppingListFooter.module.scss";
 
 const ShoppingListFooter: FC = () => {
-  return <div className={styles.container}>ShoppingListFooter</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.inputGroup}>
+        <input type="text" placeholder="Enter a name" required></input>
+        <button>Save</button>
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingListFooter;
