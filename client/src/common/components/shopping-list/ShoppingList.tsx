bbc15a6050b2efe9b66a0dacb39ebdbc812406c7
Romaric Zounlome <rzounlom@gmail.com>
2022-08-27 12:14:04 -0400

    add ShoppingtListHeader component/styles

--- client/src/common/components/shopping-list/ShoppingList.tsx
+++ client/src/common/components/shopping-list/ShoppingList.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
 import ShoppingListFooter from "./components/ShoppingListFooter";
+import ShoppingListHeader from "./components/ShoppingListHeader";
 import styles from "@styles/ShoppingList.module.scss";
+
 const ShoppingList: FC = () => {
   return (
     <div className={styles.container}>
-      ShoppingList
+      <div className={styles.itemsContainer}>
+        <ShoppingListHeader />
+      </div>
       <ShoppingListFooter />
     </div>
   );
---
+++ client/src/common/components/shopping-list/components/ShoppingListHeader.tsx
@@ -0,0 +1,43 @@
+import { FC, useState } from "react";
+
+import { FaWineBottle } from "react-icons/fa";
+import { MdModeEditOutline } from "react-icons/md";
+import { RiCheckFill } from "react-icons/ri";
+import styles from "@styles/ShoppingListHeader.module.scss";
+
+const ShoppingListHeader: FC = () => {
+  const [editTitle, setEditTitle] = useState(false);
+  const [title, setTitle] = useState("Shopping list");
+  return (
+    <div className={styles.container}>
+      <div className={styles.addItem}>
+        <div className={styles.headerIcon}>
+          <FaWineBottle size={95} />
+        </div>
+        <div className={styles.action}>
+          <span>Didn't find what you need?</span>
+          <button>Add item</button>
+        </div>
+      </div>
+      <div className={styles.itemTitle}>
+        <input
+          style={{ backgroundColor: `${editTitle ? "white" : ""}` }}
+          type="text"
+          value={title}
+          required
+          readOnly={!editTitle}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+        <div className={styles.edit} onClick={() => setEditTitle(!editTitle)}>
+          {editTitle ? (
+            <RiCheckFill size={25} color="#f9a109" />
+          ) : (
+            <MdModeEditOutline size={20} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShoppingListHeader;
